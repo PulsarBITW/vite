c921b491318a60ef057c0631c62084f6f5301ee7
PulsarBITW <GordeyBITW@gmail.com>
2025-04-12 23:21:36 +0300

    add fsd layers

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,10 @@ export default defineConfig({
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, 'src/app'),
+      '@entities': path.resolve(__dirname, 'src/entities'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@widgets': path.resolve(__dirname, 'src/widgets'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
       '@shared': path.resolve(__dirname, 'src/shared'),
     },
   },
