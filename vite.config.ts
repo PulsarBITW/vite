c23c13d6b8b0007cb74593e73b19d5650c5b674d
PulsarBITW <GordeyBITW@gmail.com>
2025-04-13 17:52:15 +0300

    add import sorting

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import {defineConfig} from 'vite';
-import path from 'path';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig} from 'vite';
 import checker from 'vite-plugin-checker';
 import svgr from 'vite-plugin-svgr';
 
